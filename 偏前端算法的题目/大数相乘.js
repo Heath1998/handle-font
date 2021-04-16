e321ed2f24e72a4df7469621661b88b4244d61d3
zehongfan <fanzehongemail@163.com>
2021-04-17 00:03:36 +0800

    2

--- 偏前端算法的题目/大数相乘.js
+++ 偏前端算法的题目/大数相乘.js
@@ -14,3 +14,24 @@ function multiply(num1, num2) {
 
 var a = multiply('789','98765');
 console.log(a);
+
+var multiply2 = function(num1, num2) {
+  if(num1=='0'||num2=='0')
+      return '0';
+  let len1 = num1.length;
+  let len2 = num2.length;
+  let res = new Array(len1+len2).fill(0);
+  num1 = num1.split('').reverse();
+  num2 = num2.split('').reverse();
+  for(let i=0;i<len1;++i)
+      for(let j=0;j<len2;++j)
+      {
+          res[i+j]+=num1[i]*num2[j];
+      }
+  for(let i=0;i<len1+len2-1;++i)
+  {
+      res[i+1] += Math.floor(res[i]/10);
+      res[i] = res[i]%10;
+  }
+  return res.reverse().join('').replace(/^0+/,'');
+};
