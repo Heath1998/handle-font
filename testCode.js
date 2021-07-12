d901d6a5a423d7e7b18bc79aa44212ff472db163
fanzehong <fanzehong@oppo.com>
2021-07-12 19:42:30 +0800

    unknow

--- testCode.js
+++ testCode.js
@@ -86,3 +86,34 @@ var c = obj.getNumLater;
 c();
 d.c = c;
 d.c();
+
+function deepClone(target, map = new WeakMap()) {
+  if (typeof target === 'object') {
+    let resTarget = target instanceof Array ? [] : {};
+    if (map.get(target)) {
+      return map.get(target);
+    }
+    map.set(target, resTarget);
+    for(var key in target) {
+      resTarget[key] = deepClone(target[key], map);
+    }
+    return resTarget;
+  } else {
+    return target;
+  }
+}
+
+
+var a = {
+  one: {
+    hello:123,
+    helloArray: [1,2,3],
+  },
+  two: 2,
+}
+
+a.three = a;
+
+var b = deepClone(a);
+console.log(b);
+// console.log(a);
