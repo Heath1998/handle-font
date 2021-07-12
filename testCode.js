95bdd158b7c069be7f8ae19e4b1abba86fa2447f
fanzehong <fanzehong@oppo.com>
2021-07-12 18:29:33 +0800

    unknow

--- testCode.js
+++ testCode.js
@@ -25,26 +25,64 @@
 // a.b()
 // a.a()
 
-var a = new Promise((resolve) => {
-  resolve(1)
-});
+// var a = new Promise((resolve) => {
+//   resolve(1)
+// });
 
-var b = a.then(() => {
-  return new Promise((resolve) => {
-    resolve('hello');
-  })
-});
+// var b = a.then(() => {
+//   return new Promise((resolve) => {
+//     resolve('hello');
+//   })
+// });
 
-b.then((value) => {
-  console.log(value);
-})
+// b.then((value) => {
+//   console.log(value);
+// })
 
 
-for(var i=0;i<5;i++) {
-  (function(j) {
-    setTimeout(() => {
-      console.log(j);
-    },1000 * j);
-  })(i)
+// for(var i=0;i<5;i++) {
+//   (function(j) {
+//     setTimeout(() => {
+//       console.log(j);
+//     },1000 * j);
+//   })(i)
 
+// }
+
+function debounce(fn, wait) {
+  let timer;
+  return function() {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    let context = this;
+    timer = setTimeout(() => {
+      fn();
+      timer = null;
+    }, wait);
+  }
 }
+
+var num = 55;
+
+function Obj (){
+    this.num = 1,
+    this.getNum = function(){
+        console.log(this.num);
+    },
+    this.getNumLater = function(){
+        setTimeout(() => {
+            console.log(this.num);
+        }, 3000)    //箭头函数中的this总是指向外层调用者，也就是Obj
+    }
+}
+var obj = new Obj; 
+// obj.getNum();//1　　打印的是obj.num，值为1
+// obj.getNumLater()//1　　打印的是obj.num，值为1
+
+var d ={num: 'fanze'}
+
+var c = obj.getNumLater;
+c();
+d.c = c;
+d.c();
